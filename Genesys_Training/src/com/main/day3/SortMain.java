f2f8d8a4ebaf161a551588403c6178f2d3f5e5a2
Diwahar Sundar <diwahar.sundar@gmail.com>
2023-01-12 18:11:14 +0530

    Day 04 Code Work

--- Genesys_Training/src/com/main/day3/SortMain.java
+++ Genesys_Training/src/com/main/day3/SortMain.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class SortMain {
 
 	public static void main(String[] args) {
-	
 		Agent agent2 = new Agent();
 		agent2.setId("XYZ123");
 		agent2.setName("Sathesh");
@@ -28,7 +27,7 @@ public class SortMain {
 		agentList.add(agent3);
 		
 		Collections.sort(agentList);
-		System.out.println(agentList);
+		//System.out.println(agentList);
 		
 		compareAge();
 		
@@ -59,9 +58,11 @@ public class SortMain {
 		agentList.add(agent1);
 		
 		
+		// Creating Objects for Inner Class
 		SortMain sortMain = new SortMain();
 		SortMain.AgeComparator agentComparator = sortMain.new AgeComparator();
 		
+		//Sorting the Collection buy using Ananymous Inner Class
 		Collections.sort(agentList, new Comparator<Agent>() {
 		
 			@Override
@@ -70,21 +71,43 @@ public class SortMain {
 			}
 		
 		});
+		
+		//Sorting the Collection buy using Ananymous Inner Class
+		/*
+		 * Collections.sort(agentList, (o1, o2) -> { o1.getAge() - o2.getAge() return
+		 * -1; });
+		 */
+		
+		Thread th = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		Thread th2 = new Thread(() -> System.out.println("Running in different thread"));
+		th2.start();
+		
+		
 		System.out.println(agentList);
 		
 		
+		//Name - function name, interface name
+		//arguments
+		//body
+		//return
+		
+		
+		//System.out.println(agentList);
 	}
-
 	
 	public class AgeComparator implements Comparator<Agent>{
-		
-		
 		@Override
 		public int compare(Agent o1, Agent o2) {
 			return o1.getAge() - o2.getAge();
 		}
-
-
 	}
 	
 	
