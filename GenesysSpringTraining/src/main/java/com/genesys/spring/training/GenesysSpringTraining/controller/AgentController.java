045afef7275b4c7529efc2129f5156a2aaa5b816
Diwahar Sundar <diwahar.sundar@gmail.com>
2023-01-25 16:39:48 +0530

    Day 04 Spring Code Work

--- GenesysSpringTraining/src/main/java/com/genesys/spring/training/GenesysSpringTraining/controller/AgentController.java
+++ GenesysSpringTraining/src/main/java/com/genesys/spring/training/GenesysSpringTraining/controller/AgentController.java
@@ -1,9 +1,10 @@
 package com.genesys.spring.training.GenesysSpringTraining.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.genesys.service.GenesysService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,24 +17,25 @@ import com.genesys.spring.training.GenesysSpringTraining.service.AgentService;
 
 @RestController
 @RequestMapping(path = "/agent")
+@ComponentScan(basePackages = "org.genesys.service")
 public class AgentController {
 
 	@Autowired
 	AgentService agentService;
 	
-	List<Agent> agentList = new ArrayList<>();
+	@Autowired
+	GenesysService genesService;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/create")
 	public String createAgent(@RequestBody Agent agent) {
 		String id = agentService.createAgent(agent);
+		agentService.saveAgent(agent);
 		return id;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Agent> getAllAgents(){
-		return this.agentList;
+		return agentService.getAllAgent();
 	}
 	
-	
-
 }
--- GenesysSpringTraining/src/main/java/com/genesys/spring/training/GenesysSpringTraining/model/Agent.java
+++ GenesysSpringTraining/src/main/java/com/genesys/spring/training/GenesysSpringTraining/model/Agent.java
@@ -1,12 +1,36 @@
 package com.genesys.spring.training.GenesysSpringTraining.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Agent implements Comparable<Agent> {
 
 	private int age;
+	
+	@Id
 	private String id;
 	private String name;
 	private String city;
 	private double salary;
+	
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "dn")
+	private DN dn;
+
+	
+
+	public DN getDn() {
+		return dn;
+	}
+
+	public void setDn(DN dn) {
+		this.dn = dn;
+	}
 
 	public double getSalary() {
 		return salary;
@@ -51,12 +75,6 @@ public class Agent implements Comparable<Agent> {
 		this.age = age;
 	}
 
-	// ABC123
-	// XYZ123
-
-	// Anantha
-	// Diwahar
-	// Satesh
 
 	@Override
 	public int compareTo(Agent o) {
@@ -67,5 +85,4 @@ public class Agent implements Comparable<Agent> {
 	public String toString() {
 		return "Agent [age=" + age + ", id=" + id + ", name=" + name + "]";
 	}
-
 }
